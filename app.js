20724727c42c9574ff568bfad48eb6bcfb4a4086
Malachi Willey <malwilley@gmail.com>
2017-04-01 22:09:00 -0400

    Added overview for project

---
+++ app.js
@@ -0,0 +1,10 @@
+// get images
+
+// get accent color
+
+// get color bins
+// sub divide across hues and lightness
+
+// populate bins with n accent color results
+
+// assign bin with largest number

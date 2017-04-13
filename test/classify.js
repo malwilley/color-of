00137e14d33c0d0819f357fd32aa5129617d86a1
Malachi Willey <malwilley@gmail.com>
2017-04-12 23:29:02 -0400

    Added mocha test framework and some initial tests

--- test/classify.js
+++ test/classify.js
@@ -1,3 +1,5 @@
+/* eslint-env node, mocha */
+
 let bingApiKey;
 
 try {
@@ -6,16 +8,34 @@ try {
   throw new Error('Must include a test/secret.js file that exports an object with a bingApiKey property');
 }
 
-const classifier = require('../src/index.js');
+const classifier = require('../lib/index.js');
+const should = require('should');
 
 const options = {
   bingApiKey,
 };
 
-classifier.classify('Cherry', options)
-.then((color) => {
-  console.log(`Term color: ${color}`);
-})
-.catch((err) => {
-  console.error(err);
+describe('classifier', () => {
+  describe('#classify()', () => {
+    it('should throw when no parameters are provided', () => {
+      (() => classifier.classify()).should.throw();
+    });
+    it('should throw when no options are provided', () => {
+      (() => classifier.classify('term')).should.throw();
+    });
+    it('should throw when no bing api key is provided', () => {
+      (() => classifier.classify('term', {})).should.throw();
+    });
+    it('should throw when term is not a string', () => {
+      (() => classifier.classify(1, options)).should.throw();
+      (() => classifier.classify({}, options)).should.throw();
+      (() => classifier.classify(true, options)).should.throw();
+      (() => classifier.classify(undefined, options)).should.throw();
+      (() => classifier.classify(null, options)).should.throw();
+      (() => classifier.classify(Symbol(''), options)).should.throw();
+    });
+    it('should throw when term is an empty string', () => {
+      (() => classifier.classify('', options)).should.throw();
+    });
+  });
 });

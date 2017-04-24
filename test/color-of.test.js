ea9734a70aa07c48dd09d77829a2bb1204c1383b
Malachi Willey <malwilley@gmail.com>
2017-04-23 23:31:31 -0400

    Fixed issue with palette generation and using numResults = 1

--- test/color-of.test.js
+++ test/color-of.test.js
@@ -9,6 +9,7 @@ try {
 const colorOf = require('../lib/index');
 const bing = require('../lib/bing');
 const matcher = require('../lib/matcher');
+const palette = require('../lib/palette');
 const Color = require('color');
 const util = require('../lib/util');
 
@@ -45,14 +46,6 @@ describe('main', () => {
       colorOf('term', options)
       .should.not.be.rejected()
       .should.eventually.be.an.instanceOf(Color);
-    });
-    it('should match "strawberry" to red', () => {
-      const promise = colorOf('strawberry', options);
-      return promise.should.finally.deepEqual(Color('red'));
-    });
-    it('should match "lime" to green', () => {
-      const promise = colorOf('lime', options);
-      return promise.should.finally.deepEqual(Color('green'));
     });
   });
 });
@@ -96,9 +89,20 @@ describe('util', () => {
 
 describe('matcher', () => {
   describe('#getHighestFrequencyColor()', () => {
-    const colors = ['#000000', '#111111', '#111111', '#222222'].map(c => Color(c));
+    const colors = ['#000000', '#111111', '#111111', '#222222']
+      .map(c => Color(c));
     it('should return the highest frequency color', () => {
       matcher.getHighestFrequencyColor(colors).hex().should.be.exactly('#111111');
+    });
+  });
+});
+
+describe('palette', () => {
+  describe('#createDefaultPalette()', () => {
+    const colors = palette.createDefaultPalette();
+    it('should return an array of 146 color objects', () => {
+      colors.should.be.instanceOf(Array).and.have.lengthOf(146);
+      colors.forEach(c => c.should.be.instanceOf(Color));
     });
   });
 });

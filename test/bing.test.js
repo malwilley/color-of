0dd242e1770112a02da6f4ac304504b6c9175f6a
Malachi Willey <malwilley@gmail.com>
2017-08-26 12:42:01 -0400

    Reorganize tests

--- test/bing.test.js
+++ test/bing.test.js
@@ -6,9 +6,9 @@ const bingApiKey = require('./utils/keys').getSecretKeys().bingApiKey;
 describe('bing', () => {
   describe('#fetchImageUrls()', () => {
     it('should return an array with [count] image urls', () => {
-      // const count = 27;
-      // return bing.fetchImageUrls(bingApiKey, 'query', count)
-      // should.eventually.be.instanceOf(Array).and.have.lengthOf(count);
+      const count = 27;
+      return bing.fetchImageUrls(bingApiKey, 'query', count)
+      .should.eventually.be.instanceOf(Array).and.have.lengthOf(count);
     });
     it('should reject invalid search query counts', () => {
       const promise = bing.fetchImageUrls(bingApiKey, 'query', -1);
--- test/color-of.test.js
+++ test/color-of.test.js
@@ -1,10 +1,4 @@
 const colorOf = require('../lib/index');
-const bing = require('../lib/bing');
-const google = require('../lib/google');
-const matcher = require('../lib/matcher');
-const imageColor = require('../lib/imageColor');
-const Color = require('color');
-const util = require('../lib/util');
 
 require('mocha-testcheck').install();
 require('should');
@@ -35,69 +29,6 @@ describe('main', () => {
     });
     it('should throw when term is an empty string', () => {
       (() => colorOf('', options)).should.throw();
-    });
-  });
-});
-
-describe('bing', () => {
-  describe('#fetchImageUrls()', () => {
-    it('should return an array with [count] image urls', () => {
-      const count = 27;
-      bing.fetchImageUrls(secret.bingApiKey, 'query', count)
-      .should.eventually.be.instanceOf(Array).and.have.lengthOf(count);
-    });
-    it('should reject invalid search query counts', () => {
-      bing.fetchImageUrls(secret.bingApiKey, 'query', -1).should.be.rejected();
-      bing.fetchImageUrls(secret.bingApiKey, 'query', 51).should.be.rejected();
-    });
-  });
-});
-
-describe('google', () => {
-  describe('#fetchImageUrls()', () => {
-    it('should return an array with [count] image urls', () => {
-      const count = 25;
-      google.fetchImageUrls(secret.google.cseId, secret.google.apiKey, 'query', count);
-    });
-  });
-});
-
-describe('util', () => {
-  describe('#colorToRgb()', () => {
-    it('should return an object with correct RGB values', () => {
-      const color = new Color('#FFFFFF');
-      const rgb = util.colorToRgb(color);
-      rgb.should.have.property('R').which.is.exactly(255);
-      rgb.should.have.property('G').which.is.exactly(255);
-      rgb.should.have.property('B').which.is.exactly(255);
-    });
-  });
-  describe('#rgbToColor()', () => {
-    it('should return the correct Color object', () => {
-      const rgb = { R: 255, G: 255, B: 255 };
-      const color = util.rgbToColor(rgb);
-      color.should.be.an.instanceOf(Color);
-      color.hex().should.be.exactly('#FFFFFF');
-    });
-  });
-});
-
-describe('matcher', () => {
-  describe('#getHighestFrequencyColor()', () => {
-    const colors = ['#000000', '#111111', '#111111', '#222222']
-      .map(c => Color(c));
-    it('should return the highest frequency color', () => {
-      matcher.getHighestFrequencyColor(colors).hex().should.be.exactly('#111111');
-    });
-  });
-});
-
-describe('imageColor', () => {
-  describe('#getImageColor()', () => {
-    it('should resolve to a color', () => {
-      imageColor.requestImageColor('https://tse4.mm.bing.net/th?id=OIP.wCXthFqx7rTL4D0F-h29mQEzDL&pid=Api')
-      .should.not.be.rejected()
-      .should.eventually.be.an.instanceOf(Color);
     });
   });
 });
